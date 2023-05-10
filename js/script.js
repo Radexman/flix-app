0c4d1c1bbc7b2cd81b6d9a60f7dcefab672af270
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-10 18:51:56 +0200

    10.05

--- js/script.js
+++ js/script.js
@@ -2,6 +2,50 @@ const global = {
 	currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+	const { results } = await fetchAPIData('movie/popular');
+
+	results.forEach((movie) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+				movie.poster_path
+					? ` <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+					: ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+			}
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+          `;
+		document.querySelector('#popular-movies').appendChild(div);
+	});
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+	const API_KEY = '157d48dcf9eb2e4fa6cf6f014980e9da';
+	const API_URl = 'https://api.themoviedb.org/3/';
+
+	const res = await fetch(`${API_URl}${endpoint}?api_key=${API_KEY}&language=en-US`);
+	const data = await res.json();
+
+	return data;
+}
+
 // Highlight active link
 function highlightActiveLink() {
 	const links = document.querySelectorAll('.nav-link');
@@ -17,7 +61,7 @@ function init() {
 	switch (global.currentPage) {
 		case '/':
 		case '/index.html':
-			console.log('Home');
+			displayPopularMovies();
 			break;
 		case '/shows.html':
 			console.log('Shows');
